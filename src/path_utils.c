2a40089c38c4bba5f2fe3a441a9187939b2ebcc5
pes2ug23cs178 <pes2ug23cs178@pesu.pes.edu>
2026-04-23 14:15:15 +0530

    updated path_utils

--- src/path_utils.c
+++ src/path_utils.c
@@ -7,32 +7,25 @@
 
 int resolve_path(const char *path, char *resolved_path) {
     struct mini_unionfs_state *st = UNIONFS_DATA;
-    char wh_path[PATH_MAX];
-    char up_path[PATH_MAX];
-    char lo_path[PATH_MAX];
+    char wh_path[PATH_MAX], up_path[PATH_MAX], lo_path[PATH_MAX];
 
-    // construct whiteout path
+    /* Whiteout Path Construction */
     char *slash = strrchr(path, '/');
-    if (slash == path) { // root file: /file.txt -> upper/.wh.file.txt
+    if (slash == path) {
         snprintf(wh_path, PATH_MAX, "%s/.wh.%s", st->upper_dir, path + 1);
-    } else { // subfolder file: /dir/file.txt -> upper/dir/.wh.file.txt
+    } else {
         int dir_len = slash - path;
         snprintf(wh_path, PATH_MAX, "%s%.*s/.wh.%s", st->upper_dir, dir_len, path, slash + 1);
     }
 
-    // If whiteout exists, the file is considered to be deleted
-    if (access(wh_path, F_OK) == 0) {
-        return -ENOENT;
-    }
+    if (access(wh_path, F_OK) == 0) return -ENOENT;
 
-    // Check upper directory
     snprintf(up_path, PATH_MAX, "%s%s", st->upper_dir, path);
     if (access(up_path, F_OK) == 0) {
         strcpy(resolved_path, up_path);
         return 0;
     }
 
-    // check lower directory
     snprintf(lo_path, PATH_MAX, "%s%s", st->lower_dir, path);
     if (access(lo_path, F_OK) == 0) {
         strcpy(resolved_path, lo_path);
